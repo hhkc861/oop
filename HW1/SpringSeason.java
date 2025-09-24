6dd49e825488ec2b3eda05f678d8dfd2e5750b62
Ha Hoang Kim Chi <hahoangkimchi1608@gmail.com>
2025-09-24 11:19:04 +0700

    bai tap o 1.2

---
+++ HW1/SpringSeason.java
@@ -0,0 +1,10 @@
+public class SpringSeason {
+    public static void main(String[] args) {
+        int day = Integer.parseInt(args[0]);
+        int month = Integer.parseInt(args[1]);
+        if((month == 3 && (day >= 20 && day <=31))||(month == 4 && (day >= 1 && day <=30))||(month == 5 && (day >= 1 && day <=30))||(month == 6 && (day >= 1 && day <=20))){
+            System.out.println("true");
+        }
+        else System.out.println("false");
+    }
+}
