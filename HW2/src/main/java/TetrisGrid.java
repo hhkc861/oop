ac968df0e5b23147a0fc9d3c3e94f81df8371724
Ha Hoang Kim Chi <hahoangkimchi1608@gmail.com>
2025-10-02 11:36:46 +0700

    bai tap TetrisGrid

--- HW2/src/main/java/TetrisGrid.java
+++ HW2/src/main/java/TetrisGrid.java
@@ -1,29 +1,62 @@
-//
-// TetrisGrid encapsulates a tetris board and has
-// a clearRows() capability.
-
 public class TetrisGrid {
-	
-	/**
-	 * Constructs a new instance with the given grid.
-	 * Does not make a copy.
-	 * @param grid
+	private boolean[][] ban;  
+
+	/**Contructor.
+	 *
 	 */
-	public TetrisGrid(boolean[][] grid) {
+	public TetrisGrid(boolean[][] ban) {
+		this.ban = ban;
 	}
-	
-	
-	/**
-	 * Does row-clearing on the grid (see handout).
+
+	/**Kiem tra.
+	 *
+	 */
+	private boolean hangDay(int h) {
+		for (int c = 0; c < ban.length; c++) {
+			if (!ban[c][h]) return false;
+		}
+		return true;
+	}
+
+	/**Dich cac hang tu y len xuong 1 bac.
+	 *
+	 *
+	 */
+	private void dichXuong(int tuHang) {
+		int cot = ban.length;
+		int hang = ban[0].length;
+
+		for (int h = tuHang; h < hang - 1; h++) {
+			for (int c = 0; c < cot; c++) {
+				ban[c][h] = ban[c][h + 1];
+			}
+		}
+
+		/**Lam trong hang tren cung.
+		 *
+		 */
+		for (int c = 0; c < cot; c++) {
+			ban[c][hang - 1] = false;
+		}
+	}
+
+	/**Xoa hang day.
+	 *
 	 */
 	public void clearRows() {
+		int hang = ban[0].length;
+		for (int h = 0; h < hang; h++) {
+			if (hangDay(h)) {
+				dichXuong(h);
+				h--;
+			}
+		}
 	}
-	
-	/**
-	 * Returns the internal 2d grid array.
-	 * @return 2d grid array
+
+	/**Tra ve bang.
+	 *
 	 */
-	boolean[][] getGrid() {
-		return null; // YOUR CODE HERE
+	public boolean[][] getGrid() {
+		return ban;
 	}
 }
